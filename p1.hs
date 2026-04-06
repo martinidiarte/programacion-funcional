37f3f3bc184daf75f733f6ea36f3c01ef34b5970
martinidiarte123-collab <martinidiarte123@gmail.com>
2026-04-06 15:52:16 -0300

    Adelanto Practico 2

--- p1.hs
+++ p1.hs
@@ -1,9 +1,11 @@
 -- ghci p1ej1.hs para compilar
 -- :r para recompilar cambios
 
+
+----------------------Ejercicio 1 --------------------------------------------------------------------------------
+--Defina una funcion sumsqrs que tome 3 numeros y retorne la suma de los cuadrados de los dos mayores
 --Auxiliares
 --Ya hay una funcion max en haskell
-
 -- Estructura para retornar por casos
 -- funcion a b c
 --  | caso1 = resultado1
@@ -17,11 +19,11 @@ mayor x y z
         | x >= y && z >= y = (x,z)
         | otherwise        = (y,z)   
 
---Ejercicio 1
---Defina una funcion sumsqrs que tome 3 numeros y retorne la suma de los cuadrados de los dos mayores
+
 sumsqrs1 :: Int -> Int -> Int -> Int 
 sumsqrs1 x y z = fst(mayor x y z )^2 + snd(mayor x y z)^2
 
+----------------------Ejercicio 2 --------------------------------------------------------------------------------
 --Defina una funcion analyze :: Int → Int → Int → Bool, que determina si tres enteros positivos son los lados de un tri´angulo.
 --Propiedad de triangulos: 
 --a + b > c 
@@ -35,7 +37,7 @@ analyze a b c =
     b + c > a
 
 
---Ejercicio 2
+----------------------Ejercicio 3 --------------------------------------------------------------------------------
 --Defina and y or usando expresiones condicionales. Haga lo mismo utilizando pattern matching
 andExp :: Bool -> Bool -> Bool
 andExp a b = a == b
@@ -46,7 +48,7 @@ andPM a b
     | a  && b = True
     | otherwise = False
 
---Ejercicio 3
+----------------------Ejercicio 4 --------------------------------------------------------------------------------
 --Defina al conectivo logico implicacion como un operador de tipo Bool.
 implica :: Bool -> Bool -> Bool
 implica a b
@@ -54,24 +56,228 @@ implica a b
     | a  && b     = True
     | otherwise   = False
 
---Ejercicio 4
+----------------------Ejercicio 5 --------------------------------------------------------------------------------
 --Supongamos que representamos fechas a traves de una tripla de enteros
 --que corresponden a dia, mes y año. Defina una funcion edad que dada
 --dos fechas, una representando la fecha de nacimiento de una persona, y la
 --otra representando la fecha actual, calcula la edad en años de la persona.  
   
-type fecha = (Dia, Mes, Anio)
+type Fecha = (Dia, Mes, Anio)
 type Dia   = Int
 type Mes   = Int
 type Anio  = Int
 
-getAnio :: fecha -> Int
+getAnio :: Fecha -> Int
 getAnio (_,_,a) = a
-geMes :: fecha -> Int
-getMes (_,_,a) = a
-getDia :: fecha -> Int
-getDia (_,_,a) = a
+getMes :: Fecha -> Int
+getMes (_,m,_) = m
+getDia :: Fecha -> Int
+getDia (d,_,_) = d
 
-edad :: fecha -> fechas -> Int
-edad f1 f2 = 
-    | getAnio f2 - getAnio f1
+edad :: Fecha -> Fecha -> Int
+edad f1 f2   
+    | getMes f1 == getMes f2 && getDia f1 <= getDia f2 = getAnio f2 - getAnio f1
+    | getMes f1 < getMes f2 = getAnio f2 - getAnio f1 
+    | otherwise  = getAnio f2 - getAnio f1 - 1
+
+----------------------Ejercicio 6 -------------------------------------------------------------------------------
+--Se desea procesar informacion relativa a estudiantes. Cada estudiante esta
+--dado por su nombre (cadena de caracteres), CI (entero), ano de ingreso
+--(entero) y lista de cursos aprobados. Cada curso est´a dado por el nombre del curso (cadena de caracteres), c´odigo del curso (entero) y nota de
+--aprobacion (entero).
+--(a) Represente la informacion de cada estudiante a traves de tuplas.
+--(b) Escriba una funcion que dado un estudiante retorne su nombre y CI.
+--(c) Escriba una funcion que dado un estudiante retorne su ano de ingreso.
+--d) Escriba una funcion que dado un estudiante y una nota retorne una
+--lista con los codigos de los cursos que aprobo con esa nota. (Sugerencia: use comprension de listas).
+--(e) Escriba una funcion que dada una lista de estudiantes retorne una
+--lista de pares (nombre, CI) de aquellos estudiantes ingresados en
+--un determinado ano dado como parametro. (Sugerencia: use comprension de listas)
+
+type Curso = (String, Int, Int) -- (NombreCurso, Codigo, Aprovacion)
+type Cursos = [Curso] 
+type Estudiante = (String, Int, Int, Cursos) -- (Nombre, CI, Anio_Ingreso, Cursos)
+
+
+-----------------------Para Pruebas-------------------------
+est1 :: Estudiante 
+est1 = ("Martin",123,2020,[("Prog",101,10),("Mat",102,8)])
+est2 :: Estudiante 
+est2 = ("Pepe",342,2020,[("Prog",101,10),("Mat",102,8)])
+est3 :: Estudiante 
+est3 = ("Nicole",542,2021,[("Prog",101,10),("Mat",102,8)])
+estudiantes :: [Estudiante]
+estudiantes = [est1,est2,est3]
+--------------------------------------------------------------
+
+getInfoEst :: Estudiante -> (String, Int)
+getInfoEst (nombre,ci,_,_) = (nombre, ci)
+
+getAnioIngreso :: Estudiante -> Int
+getAnioIngreso (_,_,anio,_) = anio
+
+getCodigo :: Curso -> Int
+getCodigo (_,c,_) = c
+
+getCodigosCursos :: Estudiante -> Int -> [Int]
+getCodigosCursos (_, _, _, cursos) nota =
+  [ codigo | (_, codigo, notaCur) <- cursos, notaCur == nota ] --[ resultado | elemento <- lista, condicion ]
+
+--significa:
+--recorrer cursos
+--desarmar cada curso en (nomCur, codigo, notaCur)
+--quedarte solo con los que cumplen notaCur == nota
+--devolver codigo
+
+--[ resultado | elemento <- lista, condicion ]
+--para cada elemento de la lista, si cumple la condición, devuelvo el resultado
+
+getIngresadosAnio :: [Estudiante] -> Int -> [(String, Int)]
+getIngresadosAnio estudiantes anio = [(nombre,ci) | (nombre,ci,a1,_) <- estudiantes, a1 == anio]
+
+----------------------Ejercicio 7--------------------------------------------------------------------------------
+--Rehaga el ejercicio anterior usando ahora tipos de datos algebraicos en lugar de tuplas.
+data CursoAlg = Curs String Int Int
+data EstudianteAlg = Est String Int Int [CursoAlg]
+
+
+--Otra forma de hacerlo para usar los nombres de los parametros
+--Usando record syntaxis
+data EstudianteAlg2 = Est2 
+  { nombre :: String
+  , ci :: Int
+  , anio :: Int
+  , cursos :: [CursoAlg]
+  }
+
+
+getInfoEstAlg :: EstudianteAlg -> (String, Int)
+getInfoEstAlg (Est nombre ci _ _) =  (nombre, ci)
+--Usando la otra forma 
+getInfoEstAlg2 :: EstudianteAlg2 -> (String, Int) 
+getInfoEstAlg2 e = (nombre e, ci e) 
+
+getAnioIngresoAlg :: EstudianteAlg -> Int
+getAnioIngresoAlg (Est _ _ anio _) = anio
+
+getCodigoAlg :: CursoAlg -> Int
+getCodigoAlg (Curs _ c _) = c
+
+getCodigosCursosAlg :: EstudianteAlg -> Int -> [Int]
+getCodigosCursosAlg (Est _ _ _ cursos) nota =
+  [ codigo | Curs _ codigo notaCur <- cursos, notaCur == nota ]  -- Constructor variables <- lista
+
+
+--Para ejecutar-----------------------------------------------
+estudiantesAlg :: [EstudianteAlg]
+estudiantesAlg =
+  [ Est "Martin" 123 2020 [Curs "Prog" 101 10, Curs "Mat" 102 8]
+  , Est "Ana"    456 2021 [Curs "Prog" 101 9]
+  , Est "Luis"   789 2020 [Curs "BD" 103 7]
+  ]
+--------------------------------------------------------------
+
+----------------------Ejercicio 8--------------------------------------------------------------------------------
+--Deseamos representar pares internamente ordenados, que son pares de
+--numeros reales (r, s) tales que r <= s
+--(a) Defina el tipo de los pares ordenados
+--(b) Defina una funci´on que dado un par de reales cualesquiera retorna
+--un par internamente ordenado.
+--(c) Defina la operaci´on de suma de pares internamente ordenados, que
+--suma las correspondientes componentes de dos pares retornando un
+--nuevo par.
+--(d) Defina la operaci´on de multiplicaci´on por un escalar, que dado un real
+--y un par internamente ordenado multiplica la primera componente
+--del par por el escalar. El resultado debe ser un par internamente ordenado. Si se pierde el orden se deben intercambiar las componentes.
+data Par = Par Float Float
+ deriving (Show, Eq)
+
+crearPar :: Float -> Float -> Par
+crearPar r s  
+    | r <= s = Par r s
+    |otherwise = Par s r
+
+sumaPares :: Par -> Par -> Par
+sumaPares (Par x1 y1) (Par x2 y2) = crearPar (x1+x2) (y1+y2) -- Constructor (exp1) (exp2) cuando tengo expresiones 
+--Pattern matching funciona con:
+--data → ✔️
+--listas (:) → ✔️
+--tuplas → ✔️
+-- pero NO con funciones
+
+productoPares :: Float -> Par -> Par
+productoPares k (Par x y)  = crearPar (k*x) (k*y)
+
+----------------------Ejercicio 9--------------------------------------------------------------------------------
+--Todo numero entero x se puede descomponer de manera unica en terminos
+--de dos numeros enteros y y z , tales que:
+-- −5 < y <= 5
+-- x = y + 10 × z.
+--Defina una funcion que dado un entero x devuelve una tupla con los
+--numeros y y z .
+
+descomposicion :: Int -> (Int, Int)
+descomposicion x = (mod x 10 , div x 10)
+
+
+----------------------Ejercicio 10 --------------------------------------------------------------------------------
+--Deseamos representar n´umeros racionales y operaciones sobre ellos. Los
+--racionales son representados por pares de enteros cuya segunda componente es distinta de cero. Cada racional tiene infinitas representaciones,
+--pero existe la llamada representaci´on can´onica en la que la segunda componente del par de enteros es mayor que cero y ambos enteros son primos
+--entre si.
+--(a) Defina el tipo racional
+--(b) Defina una funci´on que dado un par de enteros, el segundo de los
+--cuales es distinto de cero, retorne un racional en su representaci´on
+--can´onica.
+--(c) Defina las operaciones de suma, resta, multiplicaci´on, y negaci´on de
+--racionales, e int2rac, que convierte un entero en un racional. Dichas
+--operaciones deben devolver representaciones can´onicas como resultado.
+--Nota: Puede usar la funci´on gcd (definida en el Prelude) la cual
+--computa el m´aximo com´un divisor de dos n´umeros.
+
+--data Racional = Rac 
+--    {
+--    parteEntera :: Int,
+--   parteRacional :: Int 
+--    }
+--    deriving (Show, Eq)
+
+data Racional = Rac Int Int
+ deriving (Show, Eq)
+
+repCanonica :: Int -> Int -> Racional
+repCanonica x y 
+    | x /= 0 && y > 0 = Rac (div x (gcd x y)) (div y (gcd x y))
+    | x == 0 && y > 0 = Rac 0 0
+
+sumaRac :: Racional -> Racional -> Racional
+sumaRac (Rac n1 d1) (Rac n2 d2) = repCanonica (n1*d2 + n2*d1) (d1*d2)
+
+restaRac :: Racional -> Racional -> Racional
+restaRac (Rac n1 d1) (Rac n2 d2) = repCanonica (n1*d2 - n2*d1) (d1*d2)
+
+prodRac :: Racional -> Racional -> Racional
+prodRac (Rac n1 d1) (Rac n2 d2) = repCanonica (n1*n2) (d1*d2)
+
+negRac :: Racional -> Racional
+negRac (Rac n d) = repCanonica (-n) d
+
+int2rac :: Int -> Racional
+int2rac n = Rac n 1
+
+----------------------Ejercicio 11 --------------------------------------------------------------------------------
+--Dado el siguiente tipo para representar tri´angulos:
+--data Triangulo = Equi Int | Iso Int Int | Esca Int Int Int
+--Defina la funcion mkTriangulo que dados tres enteros positivos, que representan a los lados de un tri´angulo v´alido, retorna un valor de tipo
+--Triangulo.
+
+data Triangulo = Equi Int | Iso Int Int | Esca Int Int Int
+ deriving (Show, Eq)
+
+mkTriangulo :: Int -> Int -> Int -> Triangulo
+mkTriangulo l1 l2 l3  
+    | l1 == l2 && l2 == l3 = Equi l1
+    | l1 == l2 = Iso l1 l3
+    | l1 == l3 = Iso l1 l2
+    | l2 == l3 = Iso l1 l2 
+    | otherwise = Esca l1 l2 l3
--- segundo.hs
+++ segundo.hs
@@ -3,6 +3,7 @@
 --Dada la siguiente funci´on
 --dup x = (x , x )
 --Explique en que difieren (dup ◦ dup) y (dup dup).
+{- HLINT ignore "Use null" -}
 
 dup :: x -> (x, x)
 dup x = (x , x)
@@ -11,7 +12,7 @@ dup x = (x , x)
 --(dup . dup) x = dup(dup x) = dup (x,x) = ((x,x), (x,x))
 
 --aplica dup sobre dup y no sobre un tipo, entonces error
---(dup dup) x = 
+--(dup dup) x = ?
 
 ---------------------------Ejercicio 2---------------------------------
 --Dada la funcion twice
@@ -37,17 +38,14 @@ dup x = (x , x)
 
 --No la pidieron pero pinto
 sumaLista :: [Int] -> Int
-sumaLista (x : y : xs) 
-    |xs == [] = x
+sumaLista (x : y : xs)
+    |null xs = x --xs == []
     |otherwise = x + y + sumaLista xs
 
 sumaPrimeros :: [Int] -> [Int]
-sumaPrimeros ([]) = []
-sumaPrimeros (x : []) = [x] 
-sumaPrimeros (x : y : xs) 
-    | [x] /= [] && [y] /= [] = [x+y] ++ (x : y : xs)
-    | otherwise = [x]
-
+sumaPrimeros [] = []
+sumaPrimeros [x] = [x]
+sumaPrimeros (x : y : xs) = (x+y) : (x : y : xs)
 
 
 ---------------------------Ejercicio 5----------------------------------------
@@ -62,4 +60,126 @@ sumaPrimeros (x : y : xs)
 flip :: (a -> b -> c) -> b -> a -> c
 flip f x y  = f y x
 
-flip \f x y -> f y x
+--la lambda recibe 3 parámetros:
+--f   x   y
+--pero:
+--f debe ser una función
+--x e y son los valores que se le pasan a esa función
+--O sea, conceptualmente:
+--f = una función de dos argumentos
+--x = primer valor
+--y = segundo valor
+--y después hace:
+--f y x
+--intercambiando x e y
+
+flipLambda = \f x y -> f y x
+
+--ejemplo de ejecucion
+--flipLambda (-) 2 3
+
+---------------------------Ejercicio 6----------------------------------------
+
+--Usando secciones y composici´on de funciones, implemente una funci´on
+--cuentas ::Integer → Integer , que dado un n´umero, le sume 3, al resultado
+--lo multiplique por 2, luego le reste 8 y finalmente lo divida por dos.
+--Ejemplo
+-- cuentas 3 = 2
+
+cuentas :: Integer -> Integer
+--cuentas x = ((`div` 2).(+(-8)).(2*).(3+)) x --Version Humilde
+--en Haskell las funciones están “curriadas”: una composición de funciones ya produce otra función
+cuentas = (`div` 2).subtract 8.(2*).(3+)
+
+---------------------------Ejercicio 7----------------------------------------
+
+--(a) Implemente la funcion map usando listas por comprension.
+--(b) Implemente la funcion filter usando listas por comprension.
+--map :: (a → b) → [a] → [b]
+
+mapAux :: (a -> b) -> [a] -> [b]
+mapAux _ [] = []
+mapAux f (x : xs)  = f x : mapAux f xs
+
+--filter :: (a → Bool) → [a] → [a]
+
+filterAux :: (a -> Bool) -> [a] -> [a]
+filterAux _ [] = []
+filterAux f (x : xs)
+    | f x = x : filterAux f xs
+    | otherwise = filterAux f xs
+
+---------------------------Ejercicio 8----------------------------------------
+
+--Usando map, defina una funcion squares :: [Int ] → [Int ] que dada una
+--lista de enteros retorne una lista con los cuadrados de los elementos de la
+--lista.
+
+squares :: [Int] -> [Int]
+squares = map (^2) -- es lo mismo que squares x = map (^2) x
+
+---------------------------Ejercicio 9----------------------------------------
+
+--Defina la funcion length en terminos de map y sum.
+
+lengthAux :: [a] -> Int
+lengthAux = sum . map (\_ -> 1)
+
+---------------------------Ejercicio 10----------------------------------------
+
+--Usando filter , defina:
+--(a) Una funcion all :: (a → Bool) → [a] → Bool que dada una condicion
+--y una lista, verifique si todos los elementos de la lista cumplen con
+--dicha condicion. Ejemplos:
+--all (>0) [1, 2, 3] retorna True
+--all (≡ ’a’) [’a’, ’b’, ’c’] retorna False
+
+--(b) Una funcion elem :: Eq a ⇒ a → [a ] → Bool que determina si un
+--elemento pertenece a una lista. Ejemplos:
+--elem 2 [1, 2, 3] retorna True
+--elem ’a’ [’b’, ’c’] retorna False
+
+allAux :: (a -> Bool) -> [a] -> Bool
+allAux _ [] = False
+allAux cond xs = length xs == length (filter cond xs) -- Otra forma: allAux cond xs = length xs == (length .filter cond) xs
+
+elemAux :: Eq a => a -> [a] -> Bool
+elemAux a xs = length (filter (== a) xs) > 0 -- Otra forma: elemAux a xs = not (null (filter (== a) xs))
+
+
+---------------------------Ejercicio 11----------------------------------------
+
+--Indique el tipo y explique lo que hace la siguiente funcion:
+--rara p = filter p ◦ filter (not ◦ p)
+
+rara :: (a -> Bool) -> [a] -> [a]
+rara p = filter p . filter (not . p)
+
+--recibe como parametros una funcion booleana y una lista ya que filter necesita una condicion y una lista
+--Y retorna una lista ya que filter retorna una lista 
+--Lo que hace esta funcion es dada una lista primero filtra los que no cumplen la condicion p y luego a esa lista
+--restante filtra los que cumplen p, entonces retorna la lista vacia 
+
+---------------------------Ejercicio 12----------------------------------------
+
+--12. Indique el tipo y explique lo que hace la siguiente funcion:
+--rara2 = zipWith (◦) [length, sum ] [drop 4, take 4]
+--Muestre un ejemplo de aplicaci´on correcta de la expresi´on (head rara2 ) y
+--su resultado.
+
+-- drop n xs -> elimina los primeros n elementos de la lista xs
+-- take n xs -> retorna una lista con los primeros n elementos de xs
+-- zipWith op xs ys -> retorna la lista resultado de aplicar la operacion op a xs e ys elemento a elemento
+
+rara2 :: [[Int] -> Int]
+rara2 = zipWith (.) [length, sum] [drop 4, take 4]
+
+-- zipWith toma como funcion la composicion (.) y como listas a l1 = [length, sum] y l2 = [drop 4, take 4]
+-- luego obtiene [length . drop 4, sum . take 4] 
+-- Esto es una lista de funciones [a -> b]
+-- En la cabeza la funcion length.drop 4 es decir el tamaño de la lista al sacar los 4 primeros elementos
+-- Y el segundo y ultimo elemento sum.take 4 es decir la suma de los primeros 4 elementos de la lista 
+-- Como length da un numero entero la salida tiene que ser int, por lo que a y b son Int
+-- Por lo que el tipo es [[Int] -> Int]
+
+--head rara2 [1,2,3,4,5] retorna 1 ya que es la cant de elementos de la lista sacandole los 4 primeros
